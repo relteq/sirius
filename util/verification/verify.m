f5bc9e8a8ecc93c52eb3538a94459517a1095878
Elena Dorogush <dorogush@gmail.com>
2012-03-21 11:01:51 +0300

    verifier: plot flow and density error
    * freeway links only
    * output file name contains a link type

--- util/verification/verify.m
+++ util/verification/verify.m
@@ -3,7 +3,7 @@ sirius_fnam = 'output_%s_0.txt';
 sirius_conf = fullfile('conf', 'I80_sirius.xml');
 
 fprintf('Reading %s\n', aurora_fnam);
-aout = readAuroraOutput(aurora_fnam);
+aout = readAuroraOutput(aurora_fnam, 10000);
 sout = readSiriusOutput(sirius_fnam, sirius_conf);
 
 linktype = 'freeway';
@@ -25,12 +25,14 @@ sdens = bsxfun(@rdivide, sout.density(1:(end - 1), :), slanes .* sout.Links.leng
 aflow = bsxfun(@rdivide, aout.OutFlow, alanes);
 sflow = 3600 / sout.dt * bsxfun(@rdivide, sout.outflow, slanes);
 
+fnsuff = '';
 ind = strcmp(linktype, sout.Links.type);
 if any(ind)
 	adens = adens(:, ind);
 	sdens = sdens(:, ind);
 	aflow = aflow(:, ind);
 	sflow = sflow(:, ind);
+	fnsuff = ['-' linktype];
 elseif ~isempty(linktype)
 	warning('no %s links', linktype);
 end
@@ -55,19 +57,38 @@ subplot(2, 2, 4);
 adjust(pcolor(sflow));
 title('Sirius Flow, veh/hr/lane');
 colorbar;
-saveas(gcf, 'pcolor.png');
+saveas(gcf, ['pcolor' fnsuff '.png']);
 
+tend = sout.time(end - 1);
+adensend = adens(end, :);
+sdensend = sdens(end, :);
+aflowend = aflow(end, :);
+sflowend = sflow(end, :);
+denserr = 100 * abs(sdensend - adensend) ./ max(adensend, sdensend);
+denserr(adensend == 0 & sdensend == 0) = 0;
+flowerr = 100 * abs(sflowend - aflowend) ./ max(aflowend, sflowend);
+flowerr(aflowend == 0 & sflowend == 0) = 0;
 graphtitles = {'Aurora', 'Sirius'};
-figure;
-subplot(2, 1, 1);
-plot([adens(end, :); sdens(end, :)]');
-title('Density, veh/mile/lane, T = 1 day');
+
+figure('Position', [0, scrsz(2), 1024, 640]);
+subplot(2, 2, 1);
+plot([adensend; sdensend]');
+title(sprintf('Density, veh/mile/lane, T = %d sec', tend));
 legend(graphtitles{:});
 xlim([1, size(adens, 2)]);
-
-subplot(2, 1, 2);
+subplot(2, 2, 2);
+plot(denserr);
+title(sprintf('Density Error, %%, T = %d sec', tend));
+xlim([1, size(denserr, 2)]);
+ylim([0, min(100, ceil(max(denserr)))]);
+subplot(2, 2, 3);
 plot([aflow(end, :); sflow(end, :)]');
-title('Flow, veh/hr/lane, T = 1 day');
+title(sprintf('Flow, veh/hr/lane, T = %d sec', tend));
 legend(graphtitles{:});
 xlim([1, size(aflow, 2)]);
-saveas(gcf, 'compare.eps', 'psc2');
+subplot(2, 2, 4);
+plot(flowerr);
+title(sprintf('Flow Error, %%, T = %d sec', tend));
+xlim([1, size(flowerr, 2)]);
+ylim([0, min(100, ceil(max(flowerr)))]);
+saveas(gcf, ['compare' fnsuff '.eps'], 'psc2');
