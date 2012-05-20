b4d29b8a7c1543265f5e21c1944b5e1c699426a9
Elena Dorogush <dorogush@gmail.com>
2012-05-20 22:46:16 +0400

    new entry point: com.relteq.sirius.Runner
    implements "Sirius: Concept of Operations"

--- src/com/relteq/sirius/Runner.java
+++ src/com/relteq/sirius/Runner.java
@@ -6,49 +6,105 @@ package com.relteq.sirius;
 public class Runner {
 
 	/**
-	 * @param args
+	 * @param args command-line arguments
 	 */
 	public static void main(String[] args) {
-		if (0 == args.length) {
-			printUsage();
-			System.exit(-1);
-		}
-		switch (args[0]) {
-		case "import":
-		case "i":
-			break;
-		case "update":
-		case "u":
-			break;
-		case "export":
-		case "e":
-			break;
-		case "calibrate":
-		case "c":
-			break;
-		case "simulate":
-		case "s":
-			break;
-		case "simulate_output":
-		case "so":
-			break;
-		case "simulate_process":
-		case "sp":
-			break;
-		case "list_scenarios":
-		case "ls":
-			break;
-		case "list_runs":
-		case "lr":
-			break;
-		case "load":
-		case "l":
-			break;
+		try {
+			if (0 == args.length) throw new InvalidUsageException();
+			String cmd = args[0];
+			String[] arguments = new String[args.length - 1];
+			System.arraycopy(args, 1, arguments, 0, args.length - 1);
+			if (cmd.equals("import") || cmd.equals("i")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("update") || cmd.equals("u")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("export") || cmd.equals("e")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("calibrate") || cmd.equals("c")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("simulate") || cmd.equals("s")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("simulate_output") || cmd.equals("so")) {
+				com.relteq.sirius.simulator.Runner.main(arguments);
+			} else if (cmd.equals("simulate_process") || cmd.equals("sp")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("list_scenarios") || cmd.equals("ls")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("list_runs") || cmd.equals("lr")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("load") || cmd.equals("l")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("process") || cmd.equals("p")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("output") || cmd.equals("o")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("list_aggregations") || cmd.equals("la")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("link_data") || cmd.equals("ld")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("route_data") || cmd.equals("rd")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("node_data") || cmd.equals("nd")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("signal_data") || cmd.equals("sd")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("detection_data") || cmd.equals("dd")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("probe_data") || cmd.equals("pd")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("controller_data") || cmd.equals("cd")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("report") || cmd.equals("r")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("init")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("clear_data") || cmd.equals("cld")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("clear_processed") || cmd.equals("clp")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("clear_scenario") || cmd.equals("cls")) {
+				throw new NotImplementedException(cmd);
+			} else if (cmd.equals("clear_all") || cmd.equals("cla")) {
+				throw new NotImplementedException(cmd);
+			} else throw new InvalidCommandException(cmd);
+		} catch (InvalidUsageException exc) {
+			System.err.println("Usage: command [parameters]");
+			System.exit(1);
+		} catch (NotImplementedException exc) {
+			System.err.println(exc.getMessage());
+			System.exit(1);
+		} catch (InvalidCommandException exc) {
+			System.err.println(exc.getMessage());
+			System.exit(1);
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			System.exit(2);
 		}
 	}
-	
-	private static void printUsage() {
-		System.out.println("Usage: command [parameters]");
+
+	@SuppressWarnings("serial")
+	public static class NotImplementedException extends Exception {
+		/**
+		 * Constructs a <code>NotImplementedException</code> for the specified command
+		 * @param cmd name of the command
+		 */
+		NotImplementedException(String cmd) {
+			super("Command '" + cmd + "' is not implemented");
+		}
 	}
+
+	@SuppressWarnings("serial")
+	public static class InvalidCommandException extends Exception {
+		/**
+		 * Constructs an <code>InvalidCommandException</code> for the specified command
+		 * @param cmd name of the command
+		 */
+		InvalidCommandException(String cmd) {
+			super("Invalid command '" + cmd + "'");
+		}
+	}
+
+	@SuppressWarnings("serial")
+	public static class InvalidUsageException extends Exception {}
 
 }
