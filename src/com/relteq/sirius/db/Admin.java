dd4e8d7547f2fcbf5950b39dc9322c1ca4770f9f
Elena Dorogush <dorogush@gmail.com>
2012-05-26 21:27:57 +0400

    init command: load an sql script as a resource

--- src/com/relteq/sirius/db/Admin.java
+++ src/com/relteq/sirius/db/Admin.java
@@ -1,9 +1,13 @@
 package com.relteq.sirius.db;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
 import java.sql.*;
-
-import com.relteq.sirius.simulator.SiriusException;
 
 /**
  * Administers the Sirius Database
@@ -12,18 +16,14 @@ public class Admin {
 	/**
 	 * Initializes the database.
 	 * If the database exists, it is dropped and recreated.
-	 * @throws FileNotFoundException
-	 * @throws UnsupportedEncodingException
 	 * @throws SQLException
-	 * @throws SiriusException
-	 * @throws Exception
+	 * @throws IOException
 	 */
-	public static void init() throws SQLException {
+	public static void init() throws SQLException, IOException {
 		SQLExec exec = new SQLExec();
 		Parameters params = Parameters.get();
-		// TODO: add SQL scripts to the JAR
-		exec.setSrc(new File("torque" + File.separator + "sql" + File.separator + //
-				params.getDriver() + File.separator + "sirius-db-schema.sql"));
+		exec.setSrc("sql" + File.separator + //
+				params.getDriver() + File.separator + "sirius-db-schema.sql");
 		exec.setUrl(params.getUrl());
 		exec.setDriver(DriverManager.getDriver(exec.getUrl()).getClass().getName());
 		exec.setUserid(null == params.getUser() ? "" : params.getUser());
@@ -45,6 +45,48 @@ public class Admin {
 			setProject(project);
 			setTaskType("sql");
 			setTaskName("sql");
+		}
+
+		/**
+		 * Sets the resource name of the SQL script to be run
+		 * @param name the resource name
+		 * @throws IOException
+		 */
+		public void setSrc(String name) throws IOException {
+			int ind[] = {-1, -1};
+			ind[0] = name.lastIndexOf(File.separator);
+			ind[1] = name.lastIndexOf('.');
+			if (-1 == ind[1] || ind[1] <= ind[0]) ind[1] = name.length();
+			File file = File.createTempFile(name.substring(ind[0] + 1, ind[1]),//
+					ind[1] >= name.length() ? ".sql" : name.substring(ind[1]));
+			file.deleteOnExit();
+			InputStream is = Admin.class.getClassLoader().getResourceAsStream(name);
+			OutputStream os = new FileOutputStream(file);
+			copy(is, os);
+			is.close();
+			os.close();
+			super.setSrc(file);
+		}
+
+		/**
+		 * Copies an input stream to an output stream
+		 * @param is the input stream
+		 * @param os the output stream
+		 * @throws IOException
+		 */
+		private static void copy(InputStream is, OutputStream os) throws IOException {
+			ReadableByteChannel ich = Channels.newChannel(is);
+			WritableByteChannel och = Channels.newChannel(os);
+			final ByteBuffer buf = ByteBuffer.allocateDirect(16384);
+			while (ich.read(buf) != -1) {
+				buf.flip();
+				och.write(buf);
+				buf.compact();
+			}
+			buf.flip();
+			while (buf.hasRemaining()) och.write(buf);
+			ich.close();
+			och.close();
 		}
 	}
 
