17850f71d9436170b0bee901fedff0bf21a90746
Elena Dorogush <dorogush@gmail.com>
2012-06-09 20:18:06 +0400

    db service initialization & shutdown

---
+++ src/com/relteq/sirius/db/Service.java
@@ -0,0 +1,44 @@
+package com.relteq.sirius.db;
+
+import org.apache.torque.Torque;
+import org.apache.torque.TorqueException;
+
+import com.relteq.sirius.simulator.SiriusException;
+
+/**
+ * DB service initialization and shutdown
+ */
+public class Service {
+	/**
+	 * Initializes the DB service.
+	 * Connection parameters are read from the environment,
+	 * as described in the "Concept of Operations"
+	 * @throws SiriusException
+	 */
+	public static void init() throws SiriusException {
+		try {
+			Torque.init(Parameters.get().toConfiguration());
+		} catch (TorqueException exc) {
+			throw new SiriusException(exc.getMessage(), exc);
+		}
+	}
+
+	/**
+	 * @return true if the DB service is already initialized
+	 */
+	public static boolean isInit() {
+		return Torque.isInit();
+	}
+
+	/**
+	 * Shuts down the DB service
+	 * @throws SiriusException
+	 */
+	public static void shutdown() {
+		try {
+			Torque.shutdown();
+		} catch (TorqueException exc) {
+			exc.printStackTrace();
+		}
+	}
+}
