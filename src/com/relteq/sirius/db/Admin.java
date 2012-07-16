231576f43d0dad11e9d1f6cc55df06a62e3cb4a9
Elena Dorogush <dorogush@gmail.com>
2012-07-16 12:16:50 +0400

    init command revised: use torque sqlexec task

--- src/com/relteq/sirius/db/Admin.java
+++ src/com/relteq/sirius/db/Admin.java
@@ -1,15 +1,14 @@
 package com.relteq.sirius.db;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.channels.*;
 import java.sql.*;
 
+import org.apache.torque.TorqueException;
+import org.apache.torque.util.BasePeer;
+
 import com.relteq.sirius.simulator.SiriusErrorLog;
+import com.relteq.sirius.simulator.SiriusException;
 
 /**
  * Administers the Sirius Database
@@ -20,8 +19,9 @@ public class Admin {
 	 * If the database exists, it is dropped and recreated.
 	 * @throws SQLException
 	 * @throws IOException
+	 * @throws SiriusException
 	 */
-	public static void init() throws SQLException, IOException {
+	public static void init() throws SQLException, IOException, SiriusException {
 		init(Parameters.fromEnvironment());
 	}
 
@@ -30,79 +30,42 @@ public class Admin {
 	 * @param params the database connection parameters
 	 * @throws SQLException
 	 * @throws IOException
+	 * @throws SiriusException
 	 */
-	public static void init(Parameters params) throws SQLException, IOException {
+	public static void init(Parameters params) throws SQLException, IOException, SiriusException {
 		SQLExec exec = new SQLExec();
 		drop(params);
 		if (params.getDriver().equals("derby")) params.setCreate(true);
-		exec.setSrc("sql" + File.separator + //
-				params.getDriver() + File.separator + "sirius-db-schema.sql");
-		exec.setUrl(params.getUrl());
-		System.out.println("Connection URL: " + exec.getUrl());
-		exec.setDriver(DriverManager.getDriver(exec.getUrl()).getClass().getName());
-		exec.setUserid(null == params.getUser() ? "" : params.getUser());
-		exec.setPassword(null == params.getPassword() ? "" : params.getPassword());
-		SQLExec.OnError onerror = new SQLExec.OnError();
-		onerror.setValue("continue");
-		exec.setOnerror(onerror);
-		exec.setPrint(true);
-		exec.setAutocommit(true);
-		exec.execute();
+		Service.init(params);
+		exec.runStatements(new java.io.InputStreamReader(Admin.class.getClassLoader().getResourceAsStream(
+				"sql" + File.separator + params.getDriver() + File.separator + "sirius-db-schema.sql")),
+				System.err);
 	}
 
 	/**
 	 * Executes SQL statements
 	 */
-	public static class SQLExec extends org.apache.tools.ant.taskdefs.SQLExec {
+	public static class SQLExec extends org.apache.torque.task.TorqueSQLExec {
 		public SQLExec() {
 			org.apache.tools.ant.Project project = new org.apache.tools.ant.Project();
 			project.init();
 			setProject(project);
-			setTaskType("sql");
-			setTaskName("sql");
 		}
 
-		/**
-		 * Sets the resource name of the SQL script to be run
-		 * @param name the resource name
-		 * @throws IOException
-		 */
-		public void setSrc(String name) throws IOException {
-			int ind[] = {-1, -1};
-			ind[0] = name.lastIndexOf(File.separator);
-			ind[1] = name.lastIndexOf('.');
-			if (-1 == ind[1] || ind[1] <= ind[0]) ind[1] = name.length();
-			File file = File.createTempFile(name.substring(ind[0] + 1, ind[1]),//
-					ind[1] >= name.length() ? ".sql" : name.substring(ind[1]));
-			file.deleteOnExit();
-			InputStream is = Admin.class.getClassLoader().getResourceAsStream(name);
-			OutputStream os = new FileOutputStream(file);
-			copy(is, os);
-			is.close();
-			os.close();
-			super.setSrc(file);
+		@Override
+		public void runStatements(java.io.Reader reader, java.io.PrintStream out) throws IOException, SQLException {
+			super.runStatements(reader, out);
 		}
 
-		/**
-		 * Copies an input stream to an output stream
-		 * @param is the input stream
-		 * @param os the output stream
-		 * @throws IOException
-		 */
-		private static void copy(InputStream is, OutputStream os) throws IOException {
-			ReadableByteChannel ich = Channels.newChannel(is);
-			WritableByteChannel och = Channels.newChannel(os);
-			final ByteBuffer buf = ByteBuffer.allocateDirect(16384);
-			while (ich.read(buf) != -1) {
-				buf.flip();
-				och.write(buf);
-				buf.compact();
+		@Override
+		protected void execSQL(String sql, java.io.PrintStream out) {
+			try {
+				BasePeer.executeStatement(sql);
+			} catch (TorqueException exc) {
+				SiriusErrorLog.addErrorMessage(exc.getMessage());
 			}
-			buf.flip();
-			while (buf.hasRemaining()) och.write(buf);
-			ich.close();
-			och.close();
 		}
+
 	}
 
 	/**
