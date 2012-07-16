40d3c15f7fd1767fc305b543456d33e8a8ba7759
Elena Dorogush <dorogush@gmail.com>
2012-07-16 14:47:31 +0400

    db: create|drop for postgres

--- src/com/relteq/sirius/db/Admin.java
+++ src/com/relteq/sirius/db/Admin.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.*;
 
+import org.apache.log4j.Logger;
 import org.apache.torque.TorqueException;
 import org.apache.torque.util.BasePeer;
 
@@ -36,10 +37,25 @@ public class Admin {
 		SQLExec exec = new SQLExec();
 		drop(params);
 		if (params.getDriver().equals("derby")) params.setCreate(true);
+		else {
+			String dbname = params.getDBName();
+			try {
+				params.setDBName("");
+				Service.init(params);
+				BasePeer.executeStatement("CREATE DATABASE " + dbname);
+				logger.info("Database " + dbname + " created");
+			} catch (TorqueException exc) {
+				throw new SiriusException(exc);
+			} finally {
+				Service.shutdown();
+				params.setDBName(dbname);
+			}
+		}
 		Service.init(params);
 		exec.runStatements(new java.io.InputStreamReader(Admin.class.getClassLoader().getResourceAsStream(
 				"sql" + File.separator + params.getDriver() + File.separator + "sirius-db-schema.sql")),
 				System.err);
+		logger.info("Database " + params.getDBName() + " initialized");
 	}
 
 	/**
@@ -68,6 +84,8 @@ public class Admin {
 
 	}
 
+	private static Logger logger = Logger.getLogger(Admin.class);
+
 	/**
 	 * Drops the database
 	 * @param params DB connection parameters
@@ -79,5 +97,21 @@ public class Admin {
 			} catch (IOException exc) {
 				SiriusErrorLog.addErrorMessage(exc.getMessage());
 			}
+		else {
+			String dbname = params.getDBName();
+			try {
+				params.setDBName("");
+				Service.init(params);
+				BasePeer.executeStatement("DROP DATABASE IF EXISTS " + dbname);
+				logger.info("Database " + dbname + " dropped");
+			} catch (TorqueException exc) {
+				logger.error("Could not drop database " + dbname, exc);
+			} catch (SiriusException exc) {
+				logger.error(exc.getMessage(), exc);
+			} finally {
+				Service.shutdown();
+				params.setDBName(dbname);
+			}
+		}
 	}
 }
