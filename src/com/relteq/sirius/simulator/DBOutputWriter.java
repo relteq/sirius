b7e7ac9f2824ddf00fb14cc4d83430bcc728e2ba
Elena Dorogush <dorogush@gmail.com>
2012-09-16 16:27:08 +0400

    db output writer revised: no autocommit, load vehicle types in a constructor
    + str2id method added

--- src/com/relteq/sirius/simulator/DBOutputWriter.java
+++ src/com/relteq/sirius/simulator/DBOutputWriter.java
@@ -6,6 +6,8 @@ import java.util.Calendar;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.apache.torque.NoRowsException;
+import org.apache.torque.TooManyRowsException;
 import org.apache.torque.TorqueException;
 import org.apache.torque.util.Criteria;
 import org.apache.torque.util.Transaction;
@@ -17,27 +19,66 @@ import com.workingdogs.village.DataSetException;
  * Database output writer
  */
 public class DBOutputWriter extends OutputWriterBase {
-	private Connection conn = null;
-	private Long data_source_id = null;
 
 	public DBOutputWriter(Scenario scenario) {
 		super(scenario);
+		try {
+			db_scenario = ScenariosPeer.retrieveByPK(str2id(scenario.getId()));
+		} catch (NoRowsException exc) {
+			logger.error("Scenario " + str2id(scenario.getId()) + " was not found in the database");
+		} catch (TooManyRowsException exc) {
+			logger.error("Data integrity violation", exc);
+		} catch (TorqueException exc) {
+			logger.error("Could not load scenario " + str2id(scenario.getId()), exc);
+		}
+		db_vehicle_type = new VehicleTypes[scenario.getNumVehicleTypes()];
+		if (null != db_scenario) {
+			logger.info("Loading vehicle types");
+			Criteria crit = new Criteria();
+			crit.addJoin(VehicleTypesPeer.VEHICLE_TYPE_ID, VehicleTypesInSetsPeer.VEHICLE_TYPE_ID);
+			crit.add(VehicleTypesInSetsPeer.VEHICLE_TYPE_SET_ID, db_scenario.getVehicleTypeSetId());
+			try {
+				@SuppressWarnings("unchecked")
+				List<VehicleTypes> db_vt_l = VehicleTypesPeer.doSelect(crit);
+				for (VehicleTypes db_vt : db_vt_l)
+					for (int i = 0; i < scenario.getNumVehicleTypes(); ++i)
+						if (db_vt.getName().equals(scenario.getVehicleTypeNames()[i]))
+							db_vehicle_type[i] = db_vt;
+			} catch (TorqueException exc) {
+				logger.error("Failed to load vehicle types for scenario " + db_scenario.getId(), exc);
+			}
+		}
 	}
 
 	private static Logger logger = Logger.getLogger(DBOutputWriter.class);
 
 	private Scenarios db_scenario = null;
+	VehicleTypes[] db_vehicle_type;
+	private SimulationRuns db_simulation_run = null;
 
-	private void createDataSource() throws TorqueException, DataSetException {
+	private Long str2id(String id) {
+		return Long.parseLong(id, 10);
+	}
+
+	boolean success = false;
+
+	private Calendar ts = null;
+
+	@Override
+	public void open(int run_id) throws SiriusException {
+		success = false;
+		if (1 != scenario.numEnsemble)
+			logger.warn("scenario.numEnsembles != 1");
+		if (null == db_scenario)
+			throw new SiriusException("Scenario was not loaded from the database");
+
+		logger.info("Initializing simulation run");
 		Connection conn = null;
 		try {
 			conn = Transaction.begin();
 
-			if (null == db_scenario)
-				db_scenario = ScenariosPeer.retrieveByPK(Long.parseLong(getScenario().getId()), conn);
-
 			DataSources db_ds = new DataSources();
-			db_ds.setId(data_source_id = DataSourcesPeer.nextId(DataSourcesPeer.ID, conn));
+			db_ds.setId(DataSourcesPeer.nextId(DataSourcesPeer.ID, conn));
 			db_ds.save(conn);
 
 			Criteria crit = new Criteria();
@@ -46,71 +87,40 @@ public class DBOutputWriter extends OutputWriterBase {
 			final long run_number = null == max_runnum ? 1 : max_runnum.asLong() + 1;
 			logger.info("Run number: " + run_number);
 
-			com.relteq.sirius.om.SimulationRuns db_sr = new com.relteq.sirius.om.SimulationRuns();
-			db_sr.setDataSources(db_ds);
-			db_sr.setScenarios(db_scenario);
-			db_sr.setRunNumber(run_number);
-			db_sr.setVersion(com.relteq.sirius.Version.get().getEngineVersion());
-			db_sr.setBuild("");
-			db_sr.setSimulationStartTime(BigDecimal.valueOf(scenario.getTimeStart()));
-			db_sr.setSimulationDuration(BigDecimal.valueOf(scenario.getTimeEnd() - scenario.getTimeStart()));
-			db_sr.setSimulationDt(BigDecimal.valueOf(scenario.getSimDtInSeconds()));
-			db_sr.setOutputDt(BigDecimal.valueOf(scenario.getOutputDt()));
-			db_sr.setExecutionStartTime(Calendar.getInstance().getTime());
-			db_sr.setStatus(-1);
-			db_sr.save(conn);
+			db_simulation_run = new com.relteq.sirius.om.SimulationRuns();
+			db_simulation_run.setDataSources(db_ds);
+			db_simulation_run.setScenarios(db_scenario);
+			db_simulation_run.setRunNumber(run_number);
+			db_simulation_run.setVersion(com.relteq.sirius.Version.get().getEngineVersion());
+			db_simulation_run.setBuild("");
+			db_simulation_run.setSimulationStartTime(BigDecimal.valueOf(scenario.getTimeStart()));
+			db_simulation_run.setSimulationDuration(BigDecimal.valueOf(scenario.getTimeEnd() - scenario.getTimeStart()));
+			db_simulation_run.setSimulationDt(BigDecimal.valueOf(scenario.getSimDtInSeconds()));
+			db_simulation_run.setOutputDt(BigDecimal.valueOf(scenario.getOutputDt()));
+			db_simulation_run.setExecutionStartTime(Calendar.getInstance().getTime());
+			db_simulation_run.setStatus(-1);
+			db_simulation_run.save(conn);
 
 			Transaction.commit(conn);
 			conn = null;
-		} finally {
-			if (null != conn) {
-				Transaction.safeRollback(conn);
-				data_source_id = null;
-			}
-		}
-	}
-
-	java.util.Map<String, Long> vt_name2id = null;
-
-	private long getVehicleTypeId(String name, Connection conn) throws SiriusException {
-		if (null == vt_name2id) {
-			vt_name2id = new java.util.TreeMap<String, Long>();
-			logger.info("Loading vehicle type IDs");
-			Criteria crit = new Criteria();
-			crit.addJoin(VehicleTypesPeer.VEHICLE_TYPE_ID, VehicleTypesInSetsPeer.VEHICLE_TYPE_ID);
-			crit.add(VehicleTypesInSetsPeer.VEHICLE_TYPE_SET_ID, db_scenario.getVehicleTypeSetId());
-			try {
-				@SuppressWarnings("unchecked")
-				List<VehicleTypes> db_vt_l = VehicleTypesPeer.doSelect(crit, conn);
-				for (VehicleTypes db_vt : db_vt_l)
-					vt_name2id.put(db_vt.getName(), Long.valueOf(db_vt.getVehicleTypeId()));
-			} catch (TorqueException exc) {
-				throw new SiriusException(exc);
-			}
-		}
-		return vt_name2id.get(name).longValue();
-	}
-
-	@Override
-	public void open(int run_id) throws SiriusException {
-		if (1 != scenario.numEnsemble)
-			logger.warn("scenario.numEnsembles != 1");
-		try {
-			createDataSource();
-			conn = Transaction.begin();
+			success = true;
 		} catch (TorqueException exc) {
 			throw new SiriusException(exc);
 		} catch (DataSetException exc) {
 			throw new SiriusException(exc);
+		} finally {
+			if (null != conn) {
+				Transaction.safeRollback(conn);
+				db_simulation_run = null;
+			}
 		}
 		ts = Calendar.getInstance();
 		ts.set(Calendar.MILLISECOND, 0);
 	}
 
-	private Calendar ts = null;
-
 	@Override
 	public void recordstate(double time, boolean exportflows, int outsteps) throws SiriusException {
+		success = false;
 		boolean firststep = 0 == scenario.clock.getCurrentstep();
 		double invsteps = firststep ? 1.0d : 1.0d / outsteps;
 		double min = Math.floor(time / 60);
@@ -127,13 +137,14 @@ public class DBOutputWriter extends OutputWriterBase {
 				try {
 					LinkDataTotal db_ldt = fill_total(_link, exportflows, invsteps, dt);
 					fill_detailed(_link, exportflows, invsteps, dt, db_ldt.getSpeed());
-				} catch (TorqueException exc) {
+				} catch (Exception exc) {
 					throw new SiriusException(exc);
 				} finally {
 					_link.reset_cumulative();
 				}
 			}
 		}
+		success = true;
 	}
 
 	/**
@@ -143,13 +154,13 @@ public class DBOutputWriter extends OutputWriterBase {
 	 * @param invsteps
 	 * @param dt
 	 * @return the stored row
-	 * @throws TorqueException
+	 * @throws Exception
 	 */
-	private LinkDataTotal fill_total(Link link, boolean exportflows, double invsteps, double dt) throws TorqueException {
+	private LinkDataTotal fill_total(Link link, boolean exportflows, double invsteps, double dt) throws Exception {
 		LinkDataTotal db_ldt = new LinkDataTotal();
-		db_ldt.setLinkId(Long.parseLong(link.getId()));
-		db_ldt.setNetworkId(Long.parseLong(link.myNetwork.getId()));
-		db_ldt.setDataSourceId(data_source_id);
+		db_ldt.setLinkId(str2id(link.getId()));
+		db_ldt.setNetworkId(str2id(link.myNetwork.getId()));
+		db_ldt.setDataSources(db_simulation_run.getDataSources());
 		db_ldt.setTs(ts.getTime());
 		db_ldt.setAggregation("raw");
 		db_ldt.setType("mean");
@@ -189,7 +200,7 @@ public class DBOutputWriter extends OutputWriterBase {
 		if (!Double.isNaN(capdrop)) db_ldt.setCapacityDrop(new BigDecimal(capdrop));
 		FundamentalDiagram fd = link.currentFD(0);
 		if (null != fd) db_ldt.setCriticalSpeed(fd.getCriticalSpeed());
-		db_ldt.save(conn);
+		db_ldt.save();
 		return db_ldt;
 	}
 
@@ -200,16 +211,15 @@ public class DBOutputWriter extends OutputWriterBase {
 	 * @param invsteps
 	 * @param dt
 	 * @param total_speed
-	 * @throws TorqueException
-	 * @throws SiriusException
+	 * @throws Exception
 	 */
-	private void fill_detailed(Link link, boolean exportflows, double invsteps, double dt, BigDecimal total_speed) throws TorqueException, SiriusException {
-		for (int vt_ind = 0; vt_ind < scenario.getNumVehicleTypes(); ++vt_ind) {
+	private void fill_detailed(Link link, boolean exportflows, double invsteps, double dt, BigDecimal total_speed) throws Exception {
+		for (int vt_ind = 0; vt_ind < db_vehicle_type.length; ++vt_ind) {
 			LinkDataDetailed db_ldd = new LinkDataDetailed();
-			db_ldd.setLinkId(Long.parseLong(link.getId()));
-			db_ldd.setNetworkId(Long.parseLong(link.myNetwork.getId()));
-			db_ldd.setDataSourceId(data_source_id);
-			db_ldd.setVehicleTypeId(getVehicleTypeId(scenario.getVehicleTypeNames()[vt_ind], conn));
+			db_ldd.setLinkId(str2id(link.getId()));
+			db_ldd.setNetworkId(str2id(link.myNetwork.getId()));
+			db_ldd.setDataSources(db_simulation_run.getDataSources());
+			db_ldd.setVehicleTypes(db_vehicle_type[vt_ind]);
 			db_ldd.setTs(ts.getTime());
 			db_ldd.setAggregation("raw");
 			db_ldd.setType("mean");
@@ -231,37 +241,24 @@ public class DBOutputWriter extends OutputWriterBase {
 					db_ldd.setSpeed(new BigDecimal(speed));
 				}
 			}
-			db_ldd.save(conn);
+			db_ldd.save();
 		}
 	}
 
 	@Override
 	public void close() {
-		boolean success = false;
-		try {
-			Transaction.commit(conn);
-			conn = null;
-			success = true;
-		} catch (TorqueException exc) {
-			exc.printStackTrace();
-		} finally {
-			if (null != conn) {
-				Transaction.safeRollback(conn);
-				conn = null;
+		ts = null;
+		if (null != db_simulation_run) {
+			db_simulation_run.setExecutionEndTime(Calendar.getInstance().getTime());
+			db_simulation_run.setStatus(success ? 0 : 1);
+			try {
+				db_simulation_run.save();
+			} catch (Exception exc) {
+				logger.error("Failed to update simulation run status", exc);
+			} finally {
+				db_simulation_run = null;
 			}
 		}
-		ts = null;
-		if (0 != data_source_id)
-			try {
-				com.relteq.sirius.om.SimulationRuns db_sr = com.relteq.sirius.om.SimulationRunsPeer.retrieveByPK(data_source_id);
-				db_sr.setExecutionEndTime(Calendar.getInstance().getTime());
-				db_sr.setStatus(success ? 0 : 1);
-				db_sr.save();
-			} catch (Exception exc) {
-				exc.printStackTrace();
-			} finally {
-				data_source_id = null;
-			}
 	}
 
 }
