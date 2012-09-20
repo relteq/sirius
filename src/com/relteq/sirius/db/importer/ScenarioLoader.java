d425e08981031326b538e108e51486df6f301c40
Elena Dorogush <dorogush@gmail.com>
2012-09-20 11:39:27 +0400

    ScenarioLoader: removed units

--- src/com/relteq/sirius/db/importer/ScenarioLoader.java
+++ src/com/relteq/sirius/db/importer/ScenarioLoader.java
@@ -86,8 +86,6 @@ public class ScenarioLoader {
 		}
 	}
 
-	String units = null;
-
 	/**
 	 * Imports a scenario
 	 * @param scenario
@@ -96,15 +94,13 @@ public class ScenarioLoader {
 	 */
 	private Scenarios save(com.relteq.sirius.simulator.Scenario scenario) throws TorqueException, SiriusException {
 		if (null == scenario) return null;
-		com.relteq.sirius.jaxb.VehicleTypes vtypes = null;
-		if (null != scenario.getSettings()) {
-			units = scenario.getSettings().getUnits();
-			vtypes = scenario.getSettings().getVehicleTypes();
-		}
 		Scenarios db_scenario = new Scenarios();
 		db_scenario.setProjectId(getProjectId());
 		db_scenario.setName(scenario.getName());
 		db_scenario.setDescription(scenario.getDescription());
+		com.relteq.sirius.jaxb.VehicleTypes vtypes = null;
+		if (null != scenario.getSettings())
+			vtypes = scenario.getSettings().getVehicleTypes();
 		db_scenario.setVehicleTypeSets(save(vtypes));
 		db_scenario.save(conn);
 		save(scenario.getNetworkList(), db_scenario);
