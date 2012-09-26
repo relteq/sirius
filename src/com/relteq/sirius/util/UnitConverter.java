987ed9c8af096c88f2241499476b47d584899f66
Elena Dorogush <dorogush@gmail.com>
2012-09-26 12:17:54 +0400

    unit converter revised: *Converter inner classes, process(Table) method

--- src/com/relteq/sirius/util/UnitConverter.java
+++ src/com/relteq/sirius/util/UnitConverter.java
@@ -34,67 +34,119 @@ public class UnitConverter {
 		new UnitConverter(scenario).process();
 	}
 
-	private enum UnitSystem {UNKNOWN, US, METRIC};
-
 	private Scenario scenario = null;
-	private UnitSystem units;
 
 	private UnitConverter(Scenario scenario) {
 		this.scenario = scenario;
 	}
 
-	protected static final BigDecimal MILE = BigDecimal.valueOf(1609.344);
-	protected static final BigDecimal KILOMETER = BigDecimal.valueOf(1000);
-	protected static final BigDecimal HOUR = BigDecimal.valueOf(3600);
+	/** Abstract unit converter */
+	protected static abstract class Converter {
+		public enum UnitSystem {UNKNOWN, US, METRIC};
 
-	private static final MathContext mathctx = new MathContext(6, RoundingMode.HALF_UP);
+		protected static final BigDecimal MILE = BigDecimal.valueOf(1609.344);
+		protected static final BigDecimal KILOMETER = BigDecimal.valueOf(1000);
+		protected static final BigDecimal HOUR = BigDecimal.valueOf(3600);
 
-	private BigDecimal convertLength(BigDecimal length) {
-		if (null == length) return null;
-		switch (units) {
-		case US: // miles -> meters
-			return length.multiply(MILE);
-		case METRIC: // kilometers -> meters
-			return length.multiply(KILOMETER);
-		default:
-			return length;
+		protected static final MathContext mathctx = new MathContext(6, RoundingMode.HALF_UP);
+
+		public static UnitSystem getUnitSystem(String units) {
+			if (units.equalsIgnoreCase("US"))
+				return UnitSystem.US;
+			else if (units.equalsIgnoreCase("Metric"))
+				return UnitSystem.METRIC;
+			else
+				return UnitSystem.UNKNOWN;
+		}
+
+		protected UnitSystem usystem;
+
+		public Converter(UnitSystem usystem) {
+			this.usystem = usystem;
+		}
+
+		public abstract BigDecimal convert(BigDecimal value);
+	}
+
+	protected static class LengthConverter extends Converter {
+		public LengthConverter(UnitSystem usystem) {
+			super(usystem);
+		}
+
+		@Override
+		public BigDecimal convert(BigDecimal length) {
+			if (null == length) return null;
+			switch (usystem) {
+			case US: // miles -> meters
+				return length.multiply(MILE);
+			case METRIC: // kilometers -> meters
+				return length.multiply(KILOMETER);
+			default:
+				return length;
+			}
 		}
 	}
 
-	private BigDecimal convertDensity(BigDecimal density) {
-		if (null == density) return null;
-		switch (units) {
-		case US: // vehicles/mile -> vehicles/meter
-			return density.divide(MILE, mathctx);
-		case METRIC: // vehicles/kilometer -> vehicles/meter
-			return density.divide(KILOMETER);
-		default:
-			return density;
+	protected static class DensityConverter extends Converter {
+		public DensityConverter(UnitSystem usystem) {
+			super(usystem);
+		}
+
+		@Override
+		public BigDecimal convert(BigDecimal density) {
+			if (null == density) return null;
+			switch (usystem) {
+			case US: // vehicles/mile -> vehicles/meter
+				return density.divide(MILE, mathctx);
+			case METRIC: // vehicles/kilometer -> vehicles/meter
+				return density.divide(KILOMETER);
+			default:
+				return density;
+			}
 		}
 	}
 
-	private BigDecimal convertFlow(BigDecimal flow) {
-		if (null == flow) return null;
-		switch (units) {
-		case US: // vehicles/hour -> vehicles/second
-		case METRIC:
-			return flow.divide(HOUR, mathctx);
-		default:
-			return flow;
+	protected static class FlowConverter extends Converter {
+		public FlowConverter(UnitSystem usystem) {
+			super(usystem);
+		}
+
+		@Override
+		public BigDecimal convert(BigDecimal flow) {
+			if (null == flow) return null;
+			switch (usystem) {
+			case US: // vehicles/hour -> vehicles/second
+			case METRIC:
+				return flow.divide(HOUR, mathctx);
+			default:
+				return flow;
+			}
 		}
 	}
 
-	private BigDecimal convertSpeed(BigDecimal speed) {
-		if (null == speed) return null;
-		switch (units) {
-		case US: // miles/hour -> meters/second
-			return speed.multiply(MILE).divide(HOUR, mathctx);
-		case METRIC: // kilometers/hour -> meters/second
-			return speed.multiply(KILOMETER).divide(HOUR);
-		default:
-			return speed;
+	protected static class SpeedConverter extends Converter {
+		public SpeedConverter(UnitSystem usystem) {
+			super(usystem);
+		}
+
+		@Override
+		public BigDecimal convert(BigDecimal speed) {
+			if (null == speed) return null;
+			switch (usystem) {
+			case US: // miles/hour -> meters/second
+				return speed.multiply(MILE).divide(HOUR, mathctx);
+			case METRIC: // kilometers/hour -> meters/second
+				return speed.multiply(KILOMETER).divide(HOUR);
+			default:
+				return speed;
+			}
 		}
 	}
+
+	private LengthConverter lconv = null; // length converter
+	private DensityConverter dconv = null; // density converter
+	private FlowConverter fconv = null; // flow converter
+	private SpeedConverter sconv = null; // speed converter
 
 	private void process() throws SiriusException {
 		String units = null;
@@ -102,14 +154,13 @@ public class UnitConverter {
 			units = scenario.getSettings().getUnits();
 		if (null == units)
 			throw new SiriusException("no units");
-		else if (units.equalsIgnoreCase("US"))
-			this.units = UnitSystem.US;
-		else if (units.equalsIgnoreCase("Metric"))
-			this.units = UnitSystem.METRIC;
-		else {
-			this.units = UnitSystem.UNKNOWN;
+		Converter.UnitSystem usystem = Converter.getUnitSystem(units);
+		if (Converter.UnitSystem.UNKNOWN == usystem)
 			throw new SiriusException("Invalid units '" + units + "'. Should be either 'US' or 'Metric'");
-		}
+		lconv = new LengthConverter(usystem);
+		dconv = new DensityConverter(usystem);
+		fconv = new FlowConverter(usystem);
+		sconv = new SpeedConverter(usystem);
 
 		// settings: nothing to process
 		process(scenario.getNetworkList());
@@ -130,7 +181,7 @@ public class UnitConverter {
 			// node list: nothing to process
 			if (null != network.getLinkList())
 				for (Link link : network.getLinkList().getLink()) {
-					link.setLength(convertLength(link.getLength()));
+					link.setLength(lconv.convert(link.getLength()));
 				}
 		}
 	}
@@ -143,7 +194,7 @@ public class UnitConverter {
 				StringBuilder sb = new StringBuilder();
 				for (BigDecimal val : data1d.getData()) {
 					if (0 < sb.length()) sb.append(':');
-					sb.append(convertDensity(val).toPlainString());
+					sb.append(dconv.convert(val).toPlainString());
 				}
 				density.setContent(sb.toString());
 			}
@@ -159,7 +210,7 @@ public class UnitConverter {
 				StringBuilder sb = new StringBuilder();
 				for (BigDecimal val : data1d.getData()) {
 					if (0 < sb.length()) sb.append(',');
-					sb.append(convertFlow(val).toPlainString());
+					sb.append(fconv.convert(val).toPlainString());
 				}
 				cp.setContent(sb.toString());
 			}
@@ -181,18 +232,39 @@ public class UnitConverter {
 	private void process(Parameter param) {
 		if (null == param.getName() || null == param.getValue()) return;
 		final String name = param.getName();
-		String value = param.getValue();
-		BigDecimal converted = null;
+		Converter converter = null;
 		if (name.equals("capacity") || name.equals("capacity_drop") || name.endsWith("Flow"))
-			converted = convertFlow(new BigDecimal(value));
+			converter = fconv;
 		else if (name.equals("free_flow_speed") || name.equals("congestion_speed") || name.startsWith("gain"))
-			converted = convertSpeed(new BigDecimal(value));
+			converter = sconv;
 		else if (name.equals("jam_density") || name.equals("targetDensity"))
-			converted = convertDensity(new BigDecimal(value));
-		else return;
+			converter = dconv;
 		// TODO check if all the parameters are processed correctly
-		if (null != converted)
-			param.setValue(converted.toPlainString());
+		if (null != converter)
+			param.setValue(converter.convert(new BigDecimal(param.getValue())).toPlainString());
+	}
+
+	private void process(Table table) {
+		if (null == table) return;
+		Converter conv[] = new Converter[table.getColumnNames().getColumnName().size()];
+		int colnum = 0;
+		for (ColumnName colname : table.getColumnNames().getColumnName()) {
+			if (colname.getValue().equals("MeteringRates") || colname.getValue().equals("FlowThresholds"))
+				conv[colnum] = fconv;
+			else if (colname.getValue().equals("SpeedThresholds"))
+				conv[colnum] = sconv;
+			else
+				conv[colnum] = null;
+			++colnum;
+		}
+		for (Row row : table.getRow()) {
+			java.util.ListIterator<String> citer = row.getColumn().listIterator();
+			for (colnum = 0; citer.hasNext(); ++colnum) {
+				String value = citer.next();
+				if (null != conv[colnum])
+					citer.set(conv[colnum].convert(new BigDecimal(value)).toPlainString());
+			}
+		}
 	}
 
 	private void process(DemandProfileSet dpset) {
@@ -206,7 +278,7 @@ public class UnitConverter {
 					if (0 < t) sb.append(',');
 					for (int vtn = 0; vtn < data[t].length; ++vtn) {
 						if (0 < vtn) sb.append(':');
-						sb.append(convertFlow(data[t][vtn]).toPlainString());
+						sb.append(fconv.convert(data[t][vtn]).toPlainString());
 					}
 				}
 				dp.setContent(sb.toString());
@@ -219,7 +291,7 @@ public class UnitConverter {
 		for (Controller controller : cset.getController()) {
 			process(controller.getParameters());
 			process(controller.getQueueController());
-			// TODO process controller table
+			process(controller.getTable());
 		}
 	}
 
@@ -236,19 +308,20 @@ public class UnitConverter {
 	}
 
 	private void process(FundamentalDiagram fd) {
-		fd.setFreeFlowSpeed(convertSpeed(fd.getFreeFlowSpeed()));
-		fd.setCriticalSpeed(convertSpeed(fd.getCriticalSpeed()));
-		fd.setCongestionSpeed(convertSpeed(fd.getCongestionSpeed()));
-		fd.setCapacity(convertFlow(fd.getCapacity()));
+		fd.setFreeFlowSpeed(sconv.convert(fd.getFreeFlowSpeed()));
+		fd.setCriticalSpeed(sconv.convert(fd.getCriticalSpeed()));
+		fd.setCongestionSpeed(sconv.convert(fd.getCongestionSpeed()));
+		fd.setCapacity(fconv.convert(fd.getCapacity()));
 		if (null != fd.getJamDensity())
-			fd.setJamDensity(convertDensity(fd.getJamDensity()));
+			fd.setJamDensity(dconv.convert(fd.getJamDensity()));
 		if (null != fd.getCapacityDrop())
-			fd.setCapacityDrop(convertFlow(fd.getCapacityDrop()));
+			fd.setCapacityDrop(fconv.convert(fd.getCapacityDrop()));
 		if (null != fd.getStdDevCapacity())
-			fd.setStdDevCapacity(convertFlow(fd.getStdDevCapacity()));
+			fd.setStdDevCapacity(fconv.convert(fd.getStdDevCapacity()));
 		if (null != fd.getStdDevFreeFlowSpeed())
-			fd.setStdDevFreeFlowSpeed(convertSpeed(fd.getStdDevFreeFlowSpeed()));
+			fd.setStdDevFreeFlowSpeed(sconv.convert(fd.getStdDevFreeFlowSpeed()));
 		if (null != fd.getStdDevCongestionSpeed())
-			fd.setStdDevCongestionSpeed(convertSpeed(fd.getStdDevCongestionSpeed()));
+			fd.setStdDevCongestionSpeed(sconv.convert(fd.getStdDevCongestionSpeed()));
 	}
+
 }
